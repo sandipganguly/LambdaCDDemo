4376ed8fc0d2eaece4d77f7653917349c988f5fc
Gangapadhyay <gangapad@3c22fb33a973.ant.amazon.com>
2020-07-27 12:09:41 -0400

    test

--- LambdaCode/lambda_function.py
+++ LambdaCode/lambda_function.py
@@ -6,4 +6,4 @@ def lambda_handler(event, context):
     response = ec2.describe_availability_zones()
     return {"statusCode": 200, "body": json.dumps(response)}
 
-    ##dfdfssdfghh
+    ##dfdfssdfghhkk
